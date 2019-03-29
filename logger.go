427cf25c67ea225ced6804101a47ba157c436796
Lowell <lowell.mower@gmail.com>
2019-03-29 08:14:13 -0400

    Commit removes getters and setters and converts spaces to tabs
    This commit introduces changes which largely are centered around the conversion
    of spaces to tabs and whitespace fixes noted in code review. This commit also
    simplifies the DefaultLogger to be a wrapper around the LoggingInterface and to
    house the log suffix sting only.

--- logger.go
+++ logger.go
@@ -1,63 +1,44 @@
 package venom
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "time"
+	"fmt"
+	"time"
 )
 
 const (
-    TIME_FORMAT = "2006-01-02T15:04:05.999Z"
+	TIME_FORMAT = "2006-01-02T15:04:05.999Z"
+	LOG_NAME = "[venom]"
 )
 
 // LoggingInterface is the interface which any logging mechanism must satisfy
 // in order to be used as the logging mechanism for a LoggableConfigStore.
 type LoggingInterface interface {
-    Print(...interface{})
+	Print(...interface{})
 }
 
 // DefaultLogger is the struct associated with the LoggableConfigStore. It has
 // three configurable fields which ultimately compose structured log lines.
 type DefaultLogger struct {
-    log    LoggingInterface
-    Suffix string
-    Prefix string
+	log    LoggingInterface
+	suffix string
 }
 
 // NewDefaultLogger returns a reference to an instance of a DefaultLogger
 // struct with defaults for all fields.
-func NewDefaultLogger() *DefaultLogger {
-    return &DefaultLogger {
-        log:    log.New(os.Stdout, "", 0),
-        Suffix: "venom",
-        Prefix: "INFO",
-    }
+func NewLogger(l LoggingInterface) *DefaultLogger {
+	return &DefaultLogger {
+		log:    l,
+		suffix: LOG_NAME,
+	}
 }
 
 // Write takes a slice of interfaces and prints them as a structured log line
 // using the DefaultLogger's log which is a LoggingInterface.
 func (lg *DefaultLogger) Write(a ...interface{}) {
-    logLine := fmt.Sprintf("%s %s - %s: ", lg.Prefix, time.Now().UTC().Format(TIME_FORMAT), lg.Suffix)
-    for _, v := range a {
-        logLine += fmt.Sprintf(" %s", v)
-    }
+	logLine := fmt.Sprintf("%s%s: ", time.Now().UTC().Format(TIME_FORMAT), lg.suffix)
+	for _, v := range a {
+		logLine += fmt.Sprintf(" %s", v)
+	}
 
-    lg.log.Print(logLine)
-}
-
-// SetLogger takes an parameter of type LoggingInterface and sets it as the log
-// field for a DefaultLogger.
-func (lg *DefaultLogger) SetLogger(logger LoggingInterface) {
-    lg.log = logger
-}
-
-// SetPrefix takes a string and sets the DefaultLogger's Prefix field.
-func (lg *DefaultLogger) SetPrefix(s string) {
-    lg.Prefix = s
-}
-
-// SetSuffix takes a string and sets the DefaultLogger's Suffix field.
-func (lg *DefaultLogger) SetSuffix(s string) {
-    lg.Suffix = s
+	lg.log.Print(logLine)
 }
