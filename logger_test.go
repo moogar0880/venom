3d344805d8b9f2145e98645fb59f47c75ba768e8
Lowell <lowell.mower@gmail.com>
2019-03-29 08:17:46 -0400

    Commit introduces unit tests for logger.go
    This commit introduces unit test cases for the logger.go file which encompasses
    the LoggingInterface and DefaultLogger struct.

--- logger_test.go
+++ logger_test.go
@@ -1,42 +1,100 @@
 package venom
 
 import (
-    "os"
-    "log"
-    "testing"
+	"fmt"
+	"os"
+	"log"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSetLogger(t *testing.T) {
-    testIO := []struct {
-        tc   string
-        lgr  LoggingInterface
-        tipe interface{}
-    }{
-        {
-            tc:   "should be able to set a no-op logging interface",
-            lgr:  &TestLogger{},
-            tipe: &TestLogger{},
-        },
-        {
-            tc:   "should be able set to a new io.Writer (stderr)",
-            lgr:  log.New(os.Stderr, "", 0),
-            tipe: &log.Logger{},
-        },
-    }
+func TestNewLogableWith(t *testing.T) {
+	testIO := []struct {
+		tc   string
+		lgr  LoggingInterface
+	}{
+		{
+			tc:  "should be able to set a no-op logging interface",
+			lgr: &TestLogger{},
+		},
+		{
+			tc:  "should be able set to an io.Writer (stderr)",
+			lgr: log.New(os.Stderr, "", 0),
+		},
+		{
+			tc:  "should be able set to an io.Writer (stdout)",
+			lgr: log.New(os.Stdout, "", 0),
+		},
+	}
 
-    for _, test := range testIO {
-        t.Run(test.tc, func(t *testing.T) {
-            l := NewLogable()
-            defaultLog := l.GetLogger().(*log.Logger)
-            assert.IsType(t, &log.Logger{}, defaultLog)
-            
-            l.SetLogger(test.lgr)
-            customLog := l.GetLogger()
-            assert.IsType(t, test.tipe, customLog)
-        })
-    }
+	for _, test := range testIO {
+		t.Run(test.tc, func(t *testing.T) {
+			l := NewLogableWith(test.lgr)
+			assert.IsType(t, l, &Venom{})
+		})
+	}
 }
 
+func TestNewLogable(t *testing.T) {
+	testIO := []struct {
+		tc  string
+		f   func() *Venom
+		log bool
+		kv  kv
+	}{
+		{
+			tc: "should be receive a default to stdout if logable",
+			f:  NewLogable,
+			log: true,
+			kv: kv{k: "foo", v: "bar"},
+		},
+		{
+			tc: "should not log if new default store",
+			f:  New,
+			log: false,
+			kv: kv{k: "foo", v: "bar"},
+		},
+		{
+			tc: "should not log if new safe store",
+			f: NewSafe,
+			log: false,
+			kv: kv{k: "foo", v: "bar"},
+		},
+		{
+			tc: "should log to stdout with a value type int",
+			f:  NewLogable,
+			log: true,
+			kv: kv{k: "foo", v: 100},
+		},
+		{
+			tc: "should log to stdout with a value type float",
+			f:  NewLogable,
+			log: true,
+			kv: kv{k: "foo", v: 10.0},
+		},
+		{
+			tc: "should log to stdout with a value type boolean true",
+			f:  NewLogable,
+			log: true,
+			kv: kv{k: "foo", v: true},
+		},
+		{
+			tc: "should log to stdout with a value type boolean false",
+			f:  NewLogable,
+			log: true,
+			kv: kv{k: "foo", v: false},
+		},
+	}
 
+	for _, test := range testIO {
+		t.Run(test.tc, func(t *testing.T) {
+			out := redirectStdout(test)
+			if test.log {
+				assert.Contains(t, out, fmt.Sprintf("[venom]:  [%s %s]", test.kv.k, test.kv.v))
+			} else {
+				assert.Empty(t, out)
+			}
+		})
+	}
+}
